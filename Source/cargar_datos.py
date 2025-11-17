3c4b71a2a1f9d94076e950ff282465db1cdff1ec
juancollsimoes-sudo <juancollsimoes@gmail.com>
2025-11-16 20:48:39 -0400

    Update cargar_datos.py

--- Source/cargar_datos.py
+++ Source/cargar_datos.py
@@ -10,23 +10,23 @@ print("Módulo 'cargar_datos.py' cargando archivos locales...")
 
 lector = LectorDelRepositorio(dueño="", nombre_repo="") # No importa el dueño/repo aquí
 
-# Definimos los nombres de los archivos
+#Definimos los nombres de los archivos
 ingredientes_local = "Data/ingredientes.json"
 menu_local = "Data/menu.json"
 inventario_local = "Data/inventario.json"
 
-# Cargamos los datos en variables 
+#Cargamos los datos en variables 
 ingredientes = lector.cargar_archivo_local(ingredientes_local)
 hotdog_seleccionado= lector.cargar_archivo_local(menu_local)
 inventario = lector.cargar_archivo_local(inventario_local)
 
 if inventario is None:
-    # Si no existe, o está vacío/corrupto, creamos uno nuevo.
+    #Si no existe, o está vacío/corrupto, creamos uno nuevo.
     print("No se encontró 'inventario.json', se creará uno nuevo.")
     inventario = {}
 
 
-# Verificación de seguridad
+#Verificación de seguridad
 if ingredientes is None or hotdog_seleccionado is None:
     print("-------------------------------------------------")
     print("¡ERROR FATAL en cargar_datos.py!")
@@ -34,10 +34,11 @@ if ingredientes is None or hotdog_seleccionado is None:
     print("¡Asegúrate de ejecutar 'descargar_datos.py' primero!")
     print("-------------------------------------------------")
     t.sleep(2)
-    sys.exit(1) # Detiene todo si los datos no existen
+    sys.exit(1) #Detiene todo si los datos no existen
 else:
     print("Datos (INGREDIENTES, MENU Y INVENTARIO) cargados con éxito.")
 
 
 
     
+
